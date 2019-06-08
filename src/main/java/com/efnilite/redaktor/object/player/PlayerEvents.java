49700514d5ddaa2c22e8e64ed2a05fda32ffd22d
Efnilite <unconfigured@null.spigotmc.org>
2019-06-08 18:34:28 +0100

    Fix error

--- src/main/java/com/efnilite/redaktor/object/player/PlayerEvents.java
+++ src/main/java/com/efnilite/redaktor/object/player/PlayerEvents.java
@@ -1,10 +1,8 @@
 package com.efnilite.redaktor.object.player;
 
 import com.efnilite.redaktor.Redaktor;
-import com.efnilite.redaktor.object.selection.CuboidSelection;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -18,12 +16,5 @@ public class PlayerEvents implements Listener {
     @EventHandler
     public void onQuit(PlayerQuitEvent e) {
         Redaktor.getPlayerFactory().unregister(e.getPlayer());
-    }
-
-    @EventHandler
-    public void testing(PlayerCommandPreprocessEvent e) {
-        if (e.getMessage().contains("sett")) {
-            new CuboidSelection(e.getPlayer().getLocation(), e.getPlayer().getLocation().clone().add(10, 10, 10)).test();
-        }
     }
 }
