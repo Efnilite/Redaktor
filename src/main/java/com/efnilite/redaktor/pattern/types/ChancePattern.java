c1b609c8452068b28cb72bf5618f236ce169e618
Efnilite <unconfigured@null.spigotmc.org>
2019-07-30 19:58:06 +0200

    Add ChancePattern

---
+++ src/main/java/com/efnilite/redaktor/pattern/types/ChancePattern.java
@@ -0,0 +1,42 @@
+package com.efnilite.redaktor.pattern.types;
+
+import com.efnilite.redaktor.pattern.Pattern;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+
+import java.util.HashMap;
+import java.util.Random;
+
+public class ChancePattern implements Pattern {
+
+    private int sum;
+    private Random random;
+    private HashMap<Integer, BlockData> chances;
+
+    public ChancePattern() {
+        this.sum = 0;
+        this.random = new Random();
+        this.chances = new HashMap<>();
+    }
+
+    @Override
+    public BlockData apply(Block block) {
+        return chances.get(random.nextInt(sum));
+    }
+
+    /**
+     * Adds a chanced BlockData
+     *
+     * @param   data
+     *          The data
+     *
+     * @param   chance
+     *          The chance
+     */
+    public void add(BlockData data, int chance) {
+        for (int x = 0; x < chance; x++) {
+            chances.put(sum, data);
+            sum++;
+        }
+    }
+}
